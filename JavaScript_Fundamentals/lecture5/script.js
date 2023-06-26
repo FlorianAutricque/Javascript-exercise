1ba7f29ce64e08bf5e9fdb92de0a74d8d20b61cc
florian autricque <fautricque@gmail.com>
2023-06-26 12:06:24 -0400

    lecture 5

--- JavaScript_Fundamentals/lecture5/script.js
+++ JavaScript_Fundamentals/lecture5/script.js
@@ -1,6 +1,27 @@
-// reassigning a value to a variable, mutated the variable
-let age = 30;
-age = 31;
+// OPERATORS
+const now = 2040;
+const ageJonas = now - 1992;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
 
-// const variables can't be changed
-const birthYear = 1992;
+console.log(ageJonas * 2, ageJonas / 2, 2**3);
+// 2**3 means 2*2*2
+
+// concatenate strings
+const firstName = 'flo';
+const lastName = 'autcq';
+console.log(firstName + ' ' + lastName);
+
+// assignement operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+// comparison operators = to create booleans values
+console.log(ageJonas < ageSarah);
+console.log(ageSarah >= 20);
+
+const isFullAge = ageSarah >= 20;
