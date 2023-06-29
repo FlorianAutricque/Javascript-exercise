b765f431571d270cbd4cad1b4566d7977daae3ed
florian autricque <fautricque@gmail.com>
2023-06-29 16:43:09 -0400

    project 2

--- Project 02: Modal Window/script.js
+++ Project 02: Modal Window/script.js
@@ -5,14 +5,14 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-
 const openModal = function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++)
@@ -20,3 +20,11 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
+      closeModal();
+    }
+  }
+});
