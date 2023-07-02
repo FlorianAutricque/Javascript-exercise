3fa2e1b5d8489cff6d82c1dc95194774489e3877
florian autricque <fautricque@gmail.com>
2023-07-02 11:53:22 -0400

    project 3

--- Project 03: Pig Game/script.js
+++ Project 03: Pig Game/script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 // Selecting elements
+const player0Element = document.querySelector(".player--0");
+const player1Element = document.querySelector(".player--1");
 const score0Element = document.querySelector("#score--0");
 const score1Element = document.getElementById("score--1");
 const currentScore0Elment = document.getElementById("current--0");
@@ -14,6 +16,7 @@ score1Element.textContent = 0;
 diceElement.classList.add("hidden");
 
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dicee
 rollDice.addEventListener("click", function () {
@@ -23,6 +26,14 @@ rollDice.addEventListener("click", function () {
 
   if (dice !== 1) {
     currentScore += dice;
-    currentScore0Elment.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    // add the class or remove it if it's not there
+    player0Element.classList.toggle("player--active");
+    player1Element.classList.toggle("player--active");
   }
 });
