00bc6e5e45592e3f9860450859499ccdc8611a0e
florian autricque <fautricque@gmail.com>
2023-07-02 13:42:16 -0400

    project 3

--- Project 03: Pig Game/script.js
+++ Project 03: Pig Game/script.js
@@ -9,31 +9,79 @@ const currentScore0Elment = document.getElementById("current--0");
 const currentScore1Elment = document.getElementById("current--1");
 const diceElement = document.querySelector(".dice");
 const rollDice = document.querySelector(".btn--roll");
+const holdBtn = document.querySelector(".btn--hold");
+const newBtn = document.querySelector(".btn--new");
+
+let scores, currentScore, activePlayer, playing;
 
 // Starting conditions
-score0Element.textContent = 0;
-score1Element.textContent = 0;
-diceElement.classList.add("hidden");
+const initialise = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-let currentScore = 0;
-let activePlayer = 0;
+  score0Element.textContent = 0;
+  score1Element.textContent = 0;
+  currentScore0Elment.textContent = 0;
+  currentScore1Elment.textContent = 0;
 
-// Rolling dicee
+  diceElement.classList.add("hidden");
+  player0Element.classList.remove("player--winner");
+  player1Element.classList.remove("player--winner");
+  player0Element.classList.add("player--active");
+  player1Element.classList.remove("player--active");
+};
+initialise();
+
+// switch function
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  // add the class or remove it if it's not there
+  player0Element.classList.toggle("player--active");
+  player1Element.classList.toggle("player--active");
+};
+
+// Rolling dice
 rollDice.addEventListener("click", function () {
-  const dice = Math.trunc(Math.random() * 6 + 1);
-  diceElement.classList.remove("hidden");
-  diceElement.src = `images/dice-${dice}.png`;
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6 + 1);
+    diceElement.classList.remove("hidden");
+    diceElement.src = `images/dice-${dice}.png`;
 
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    // add the class or remove it if it's not there
-    player0Element.classList.toggle("player--active");
-    player1Element.classList.toggle("player--active");
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
   }
 });
+
+holdBtn.addEventListener("click", function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceElement.classList.add("hidden");
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+      console.log(scores);
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+newBtn.addEventListener("click", initialise);
