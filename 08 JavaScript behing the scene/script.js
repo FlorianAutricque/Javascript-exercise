c9df6f13a514124b68a49dffcfa7863ac765bf5f
florian autricque <fautricque@gmail.com>
2023-07-03 11:18:39 -0400

    JS behing the scene

--- 08 JavaScript behing the scene/script.js
+++ 08 JavaScript behing the scene/script.js
@@ -8,8 +8,12 @@ function calcAge(birthYear) {
     const output = `${firstName} You are ${age}, born in ${birthYear}`;
     console.log(output);
 
-    // new block scop
+    // new block scope
     if (birthYear >= 1981 && birthYear <= 1996) {
+      // creating new variable with the same name as the one outer the scope
+      // it prints out didi in this scope because JS looks for the 1st one
+      const firstName = "didi";
+
       const string = `Oh and you're a millenial, ${firstName}`;
       console.log(string);
     }
@@ -19,17 +23,17 @@ function calcAge(birthYear) {
   return age;
 }
 
-// the scop of the vairable age is all the function calcAge
+// the scope of the vairable age is all the function calcAge
 // printAge is a child scop
 
 // firstName is a global variable
 // JS couldn't find the FN variable so it did a lookup
-// in a scop chain
+// in a scope chain
 
-// firstName is found because js goes in the global scop
+// firstName is found because js goes in the global scope
 
 const firstName = "Flo";
 calcAge(1992);
 
 // if I try to log here age it won't work because I'm outside of
-// the parent scop, same for printAge
+// the parent scope, same for printAge
