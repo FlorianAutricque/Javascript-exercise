a8c18d068cd8efe10d81a05316574be475e0225a
florian autricque <fautricque@gmail.com>
2023-07-04 14:36:11 -0400

    data structure

---
+++ 09 Data structures, modern operators and strings/09 Enhanced Object Literals/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const weekdays = ["mon", "thu", "wed", "thu", "fri", "sat", "sun"];
+
+const openningHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0,
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  //ES6 inheance litteral
+  openningHours,
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = "22:00", address }) {
+    console.log(
+      `Order received, ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address}, at ${time}`
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(`Here is your order with ${ing1}, ${ing2} and ${ing3}`);
+  },
+};
