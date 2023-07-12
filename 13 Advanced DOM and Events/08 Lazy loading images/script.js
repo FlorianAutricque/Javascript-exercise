b57dfe82553e0d63b313b5ac7b5de14d68ed9127
florian autricque <fautricque@gmail.com>
2023-07-12 14:24:02 -0400

    lazy loading

---
+++ 13 Advanced DOM and Events/08 Lazy loading images/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// LAZY LOADING IMAGES
+
+//have a lower qlty img, then blur it in the css
+// have a data-src of higher qlty img
+
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
--- 13 Advanced DOM and Events/Bankist/script.js
+++ 13 Advanced DOM and Events/Bankist/script.js
@@ -148,7 +148,6 @@ const allSection = document.querySelectorAll(".section");
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) return;
 
@@ -165,3 +164,28 @@ allSection.forEach(function (section) {
   sectionObs.observe(section);
   section.classList.add("section--hidden");
 });
+
+// lazy loading images
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
