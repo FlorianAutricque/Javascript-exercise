30a089b585316626d916bdf09d7d3688b0e060db
florian autricque <fautricque@gmail.com>
2023-07-13 14:54:56 -0400

    lecture

--- 14 OOP/01 Constructor Functions/script.js
+++ 14 OOP/01 Constructor Functions/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+//coonstructor function
+const Person = function (firstName, birthYear) {
+  //instance propreties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+// to call a CF we need 'new'
+const flo = new Person("flo", 1234);
+console.log(flo);
+
+const matilda = new Person("matilda", 1334);
+
+console.log(matilda instanceof Person);
