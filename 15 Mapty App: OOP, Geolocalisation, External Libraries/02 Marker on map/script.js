ca52dffacc1c04c2f2d867e231d3df160cc4d20b
florian autricque <fautricque@gmail.com>
2023-07-14 15:50:59 -0400

    map marker

---
+++ 15 Mapty App: OOP, Geolocalisation, External Libraries/02 Marker on map/script.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+// if (navigation.geolocation)
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+
+    const map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>',
+    }).addTo(map);
+
+    //eventlistener that is made from leaflet
+    map.on("click", function (mapEvent) {
+      console.log(mapEvent);
+
+      const { lat, lng } = mapEvent.latlng;
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          //market is made here, fin on documentation of leaflet
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: "running-popup",
+          })
+        )
+        //popup
+        .setPopupContent("Workout")
+        .openPopup();
+    });
+  },
+  function () {
+    alert("Could not get your position");
+  }
+);
