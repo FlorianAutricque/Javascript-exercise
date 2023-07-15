7797912005995037388175fea15d3254498b5d62
florian autricque <fautricque@gmail.com>
2023-07-15 16:17:20 -0400

    classes

--- 15 Mapty App: OOP, Geolocalisation, External Libraries/Project /script.js
+++ 15 Mapty App: OOP, Geolocalisation, External Libraries/Project /script.js
@@ -11,8 +11,50 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-let map, mapEvent;
+class Workout {
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
 
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // minutes per km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 256);
+const cycl1 = new Cycling([39, -12], 34, 54, 556);
+console.log(run1, cycl1);
+
+///////////////////////////////////////////////////////////////////
+//APP
 class App {
   #map;
   #mapEvent;
