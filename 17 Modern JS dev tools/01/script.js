1de0957624042f26762ce1ff5234144488ef7dea
florian autricque <fautricque@gmail.com>
2023-07-18 16:39:45 -0400

    top level await

--- 17 Modern JS dev tools/01/script.js
+++ 17 Modern JS dev tools/01/script.js
@@ -12,12 +12,31 @@
 
 // // console.log(shippingCost); // not definied
 
-console.log("importing module");
+// console.log("importing module");
 
-import * as ShoppingCart from "./shoppingCart.js";
-ShoppingCart.addToCart("bread", 5);
-console.log(ShoppingCart.totalPrice);
+// import * as ShoppingCart from "./shoppingCart.js";
+// ShoppingCart.addToCart("bread", 5);
+// console.log(ShoppingCart.totalPrice);
 
-//import the default function
-import add from "./shoppingCart.js";
-add("pizza", 2);
+// //import the default function
+// import add from "./shoppingCart.js";
+// add("pizza", 2);
+
+//TOP LEVEL AWAIT
+// const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+// const data = await res.json();
+// console.log(data);
+
+const getLastPost = async function () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  // console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost); // promise
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2); //actual title and body
