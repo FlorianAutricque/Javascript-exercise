fbf2453a7ad802bb6d8025349155283957559007
florian autricque <fautricque@gmail.com>
2023-07-20 16:52:13 -0400

    search result

--- 18 Forkify/app/src/js/controller.js
+++ 18 Forkify/app/src/js/controller.js
@@ -2,17 +2,20 @@ import { async } from "regenerator-runtime";
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
 
     if (!id) return;
-    //lodaing spinner
-    recipeView.renderSpinner();
 
     // loading recipe
     await model.loadRecipe(id);
@@ -26,6 +29,9 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    //render spinner
+    resultsView.renderSpinner();
+
     //get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -34,7 +40,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     //render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
